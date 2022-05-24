c20e2766cdf5c3a4f5278fc098ca2e6dbec391a5
saman-barakat <saman.barakat@gmail.com>
2022-05-24 19:35:55 +0200

    Code updated

--- src/main/java/eu/asaitec/product/Product.java
+++ src/main/java/eu/asaitec/product/Product.java
@@ -24,4 +24,12 @@ public class Product {
     public void setPrice(double price) {
         this.price = price;
     }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
